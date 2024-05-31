88af700741bd6aa1a37104b1bc800ce467208b93
sianchilcott <43378067+sianchilcott@users.noreply.github.com>
2024-05-31 13:05:01 +1000

    Update SIA_max_Calibration_Publication.m

--- SIA_max_Calibration_Publication.m
+++ SIA_max_Calibration_Publication.m
@@ -10,34 +10,14 @@ d = ppp(4);
 g = ppp(5);
 
 
-% y_SIA_max = f .* -exp(sin(x .* a - e)) + sin(x.^g) + (f*d);
-% y_SIA_max = f .* (-exp(sin(x .* a - e))) + sin(x.^g) + (f*d);
-% y_SIA_max = f .* (-exp(sin((x.^g) .* a - e))) + (f*d );
+% SIA_max parameterisation
 y_SIA_max = f .* (-exp(sin((x.^g) .* a - e))) + d;
 
 
+% Calculate the residual sum of the squared di↵erences (RSS) to find the global miuimum of each parameter
 A = (SIA_max_cmip6 - y_SIA_max).^2;
 obj = sum(A);
 obj = mean(obj);
 
 end
 
-
-%%  2.0
-
-% function obj = SIA_max_Calibration_Publication(ppp, x, SIA_max_cmip6)
-% 
-% f = ppp(1);
-% a = ppp(2);
-% d = ppp(3);
-% g = ppp(4);
-% 
-% 
-% y_SIA_max = f .* (-exp(sin(x .* a))) + sin(x.^g) + (f*d);
-% 
-% 
-% A = (SIA_max_cmip6 - y_SIA_max).^2;
-% obj = sum(A);
-% obj = mean(obj);
-% 
-% end
